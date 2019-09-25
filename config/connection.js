1ded0ed17a4077a809b13f85fff02d6a8c08d990
Jenvoss17 <50814565+Jenvoss17@users.noreply.github.com>
2019-09-25 17:48:56 -0400

    add code for jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,18 @@
 var mysql = require("mysql");
+var connection;
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: process.env.DATABASE_HOST,
+        port: process.env.DATABASE_PORT,
+        user: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASS,
+        database: process.env.DATABASE_NAME,
+        multipleStatements: true
+    });
+}
 
-// Set up our connection information
-console.log(process.env.DATABASE_NAME)
-var connection = mysql.createConnection({
-    host: process.env.DATABASE_HOST,
-    port: process.env.DATABASE_PORT,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASS,
-    database: process.env.DATABASE_NAME,
-    multipleStatements: true
-});
 
 // Connect to the database
 connection.connect(function (err) {
